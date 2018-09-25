5ea45c8bfb7356ba35de5a09069877dba3d5001f
root <root@ubnt>
2018-09-25 20:29:30 +1000

    restore vnstat backup functionality

--- vnstat-update.sh
+++ vnstat-update.sh
@@ -4,3 +4,14 @@ cd /config/scripts/vnmon/
 git pull
 
 python ./vnstat-update.py
+
+mkdir vnstat-data	
+currenttime=$(date +%H:%M)	
+if [[ "$currenttime" = "06:20" ]]; then	
+  cp -Rf /var/log/vnstat ./vnstat-data
+  git add .
+  git commit -m 'daily vnstat backup'
+  git push
+fi
+
+
